b61d4eb6f039b1565980f2f42c30fb335fdf19c2
josereyes <joseluisreyes0858@gmail.com>
2024-01-21 01:08:10 -0500

    Ocultar menu al seleccionar una sección

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelector(".nav-link");
 
-
-hamburger.addEventListener("click", () =>{
+hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("activate");
     navMenu.classList.toggle("activate");
-})
+});
 
-document.querySelector(".nav-link").forEeach(n => n.addEventListener("clic", () =>{
+navMenu.addEventListener("click", () =>{
     hamburger.classList.remove("activate");
     navMenu.classList.remove("activate");
+})
 
-}))
-  
