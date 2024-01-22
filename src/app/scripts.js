404d219ab1d1d02f4f64705a44dbc9f4a64794e1
josereyes <joseluisreyes0858@gmail.com>
2024-01-21 20:53:17 -0500

    Limpieza de archivos innecesarios

--- src/app/scripts.js
+++
@@ -1,5 +0,0 @@
-const btnMenu = document.querySelector("#btnMenu");
-const main_menu = document.querySelector("#main_menu");
-btnMenu.addEventListener("click", function(){
-    main_menu.classList.toggle("mostrar");
-});
